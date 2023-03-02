7948509a457e5d37d8e9d0b3cd35e6f52a2b6566
StevenZhang0116 <zz2589@nyu.edu>
2023-03-01 22:48:58 -0500

    login-page-finish

---
+++ front-end/src/Pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginSub from '../Components/Login/loginsub'
+
+const Login = () => {
+    return (
+      <div>
+        <LoginSub/>
+      </div>
+    )
+}
+  
+export default Login
