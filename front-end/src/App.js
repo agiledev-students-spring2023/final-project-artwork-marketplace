e805a5a352a29c40cd72c9dfd7f06c6f7fd0f01d
StevenZhang0116 <zz2589@nyu.edu>
2023-03-02 23:08:47 -0500

    addstate-and-submithandle

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import AboutUs from './Pages/AboutUs'
--- front-end/src/Components/Login/loginsub.jsx
+++ front-end/src/Components/Login/loginsub.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './loginsub.css'
 import BlueWave from '../../Assets/Pictures/blue-wave.jpeg'
  
-const loginsub = () => {
+const Loginsub = () => {
+    const [theemail,setTheemail] = useState("")
+    const [thepassword,setThepassword] = useState("")
+
+    function handleSubmit(event){
+        event.preventDefault()
+        /* test */ 
+        console.log('SUBMIT TEST')
+        console.log('email=',theemail)
+        console.log('password=',thepassword)
+    }
+
     return(
         <div className="bodyset">
             <div className="photoimg">
@@ -15,12 +26,15 @@ const loginsub = () => {
                 Login
             </h2>
             <div className="loginset">        
-                <input type='text' placeholder='Email'/>
-                <input type='text' placeholder='Password'/>
-                <input type='submit' value='Login' />  
+                <input type='text' placeholder='Email' value={theemail} onChange={(e) => setTheemail(e.target.value)} />
+                <input type='text' placeholder='Password' value={thepassword} onChange={(e) => setThepassword(e.target.value)} />
+                <form onSubmit={handleSubmit}>
+                    <input type='submit' value='Login' />  
+                </form>
             </div>
         </div>
     )
 }
 
-export default loginsub
+export default Loginsub
+
