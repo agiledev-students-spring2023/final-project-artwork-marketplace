45b8b391aceeac653a509c73b3f4f04257d8d2c2
ay1534 <ay52401@gmail.com>
2023-03-04 07:47:12 -0500

    Test Landing

---
+++ front-end/src/Pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutMembers from '../Components/AboutUs/aboutMembers'
+import AboutProject from '../Components/AboutUs/aboutProject'
+
+const Landing = () => {
+  return (
+    <div>
+        <AboutProject/>
+        <AboutMembers/>
+    </div>
+  )
+}
+
+export default Landing
