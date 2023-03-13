8dfb2321f2f888c9eabfaee5922b0cb179ff038c
Anhtrr <at3947@nyu.edu>
2023-03-13 18:45:39 -0400

    finished category page

--- front-end/src/Components/CustomerHomePage/CategoryDisplay.jsx
+++ front-end/src/Components/CustomerHomePage/CategoryDisplay.jsx
@@ -5,6 +5,7 @@ import { Pagination, FreeMode } from "swiper"
 import './categoryDisplay.css'
 import "swiper/css"
 import "swiper/css/pagination"
+import randomFakeData from './randomFakeData'
 
 const CategoryDisplay = props => {
   const [categories, setCategories] = useState([])
@@ -27,129 +28,6 @@ const CategoryDisplay = props => {
     }
     setSearchValue(e.target.value)
   }
-
-  const [randomFakeData, setRandomFakeData] = useState([
-    {
-        id: 1,
-        category: "modern",
-        products: [
-            {
-                id: 11,
-                productName: "product1aiushdiuashdiuashdiaushdiuashdiuashdiashdiusah",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/400/200",
-            },
-            {
-                id: 12,
-                productName: "product2",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/300/500",
-            },
-            {
-                id: 13,
-                productName: "product3",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/400",
-            },
-            {
-                id: 14,
-                productName: "product4",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/500/350",
-            },
-            {
-                id: 15,
-                productName: "product5",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/700/300",
-            },
-            {
-                id: 16,
-                productName: "product6",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/200/300",
-            },
-        ],
-    },
-    {
-        id: 2,
-        category: "realism",
-        products: [
-            {
-                id: 21,
-                productName: "product1aiushdiuashdiuashdiaushdiuashdiuashdiashdiusah",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/400/200",
-            },
-            {
-                id: 22,
-                productName: "product2",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/300/500",
-            },
-            {
-                id: 23,
-                productName: "product3",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/400",
-            },
-            {
-                id: 24,
-                productName: "product4",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/500/350",
-            },
-            {
-                id: 25,
-                productName: "product5",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/700/300",
-            },
-            {
-                id: 26,
-                productName: "product6",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/200/300",
-            },
-        ],
-    },
-    {
-        id: 3,
-        category: "classic",
-        products: [
-            {
-                id: 31,
-                productName: "product1aiushdiuashdiuashdiaushdiuashdiuashdiashdiusah",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/400/200",
-            },
-            {
-                id: 32,
-                productName: "product2",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/300/500",
-            },
-            {
-                id: 33,
-                productName: "product3",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/400",
-            },
-            {
-                id: 34,
-                productName: "product4",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/500/350",
-            },
-            {
-                id: 35,
-                productName: "product5",
-                productArtist: "Artist Name",
-                imageURL: "https://picsum.photos/700/300",
-            },
-        ]
-    },
-    ])
   
   return (
     <>
--- front-end/src/Pages/CategoryPage.js
+++ front-end/src/Pages/CategoryPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ShopperNavbar from '../Components/Navbar/ShopperNavbar'
+import ProductDisplay from '../Components/CategoryPage/ProductDisplay'
 
 const Category = () => {
     return (
       <div>
+        <ProductDisplay/>
         <ShopperNavbar/>
       </div>
     )
