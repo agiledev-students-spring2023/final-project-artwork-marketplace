28bdfcd57ab7041275bbeace677180bf50136114
Anhtrr <at3947@nyu.edu>
2023-03-25 18:23:23 -0400

    setup backend folder

---
+++ back-end/app.js
@@ -0,0 +1,21 @@
+const express = require("express") 
+const app = express()
+const path = require("path")
+const multer = require("multer") 
+const axios = require("axios")
+require("dotenv").config({ silent: true })
+const morgan = require("morgan")
+
+const artworksRoute = require("./Routes/artworks")
+const categoriesRoute = require("./Routes/categories")
+const usersRoute = require("./Routes/users")
+
+app.use(morgan("dev"))
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
+app.use("/static", express.static("public"))
+app.use('/artworks', artworksRoute)
+app.use('/categories', categoriesRoute)
+app.use('/users', usersRoute)
+
+module.exports = app 
---
+++ back-end/server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const server = require("./app")
+
+const port = process.env.PORT || 3001
+
+const listener = server.listen(port, function () {
+  console.log(`Server running on port: ${port}`)
+})
+
+const close = () => {
+  listener.close()
+}
+
+module.exports = {
+  close: close,
+}
