50a08e80a1ce22bcfc42b6d52c5b6543d7303b21
Anhtrr <at3947@nyu.edu>
2023-03-26 00:37:40 -0400

    artwork routes done

---
+++ back-end/SchemaSamples/AllCategories.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    "_id": 1,
+    "name": "Modern",
+    "products_id": [
+        1,
+        3,
+        9,
+        11,
+        15
+    ]
+  }, 
+  {
+    "_id": 2,
+    "name": "Classic",
+    "products_id": [
+        2,
+        3,
+        4,
+        5,
+        6,
+        9,
+        10,
+        12,
+        14
+    ]
+  }, 
+  {
+    "_id": 3,
+    "name": "Contemporary",
+    "products_id": [
+        1,
+        5,
+        6,
+        7,
+        8,
+        12,
+        13,
+        14
+    ]
+  }
+]
---
+++ back-end/SchemaSamples/AllProducts.js
@@ -0,0 +1,238 @@
+module.exports = [
+  {
+    "_id": 1,
+    "artist_id": 2,
+    "name": "Furbish's Lousewort",
+    "shortDescription": "ut labore et dolore magna aliqua",
+    "price": 1549,
+    "status": "sold",
+    "thumbnailURL": "https://picsum.photos/700/300",
+    "categories_id": [
+      3,
+      1
+    ],
+    "imagesURL": [
+      "https://picsum.photos/500",
+      "https://picsum.photos/1000/1200",
+      "https://picsum.photos/1000/400"
+    ]
+  }, {
+    "_id": 2,
+    "artist_id": 3,
+    "name": "Woolly Desertdandelion",
+    "shortDescription": "sed do eiusmod tempor incididunt",
+    "price": 2792,
+    "status": "sold",
+    "thumbnailURL": "https://picsum.photos/400",
+    "categories_id": [
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/500",
+      "https://picsum.photos/1000/400"
+    ]
+  }, {
+    "_id": 3,
+    "artist_id": 3,
+    "name": "Alpine Bedstraw",
+    "shortDescription": "consectetur adipiscing elit",
+    "price": 1379,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/1000/1200",
+    "categories_id": [
+      2,
+      1
+    ],
+    "imagesURL": [
+      "https://picsum.photos/1000/1200",
+      "https://picsum.photos/200/300"
+    ]
+  }, {
+    "_id": 4,
+    "artist_id": 3,
+    "name": "Virginia Strawberry",
+    "shortDescription": "sed do eiusmod tempor incididunt",
+    "price": 3028,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/700/300",
+    "categories_id": [
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/500/350",
+      "https://picsum.photos/300/500",
+      "https://picsum.photos/1000/1200"
+    ]
+  }, {
+    "_id": 5,
+    "artist_id": 2,
+    "name": "Professor-weed",
+    "shortDescription": "ut labore et dolore magna aliqua",
+    "price": 4252,
+    "status": "sold",
+    "thumbnailURL": "https://picsum.photos/300/500",
+    "categories_id": [
+      3,
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/200/300",
+      "https://picsum.photos/500/350"
+    ]
+  }, {
+    "_id": 6,
+    "artist_id": 3,
+    "name": "Bishop's Hat",
+    "shortDescription": "ut labore et dolore magna aliqua",
+    "price": 4991,
+    "status": "sold",
+    "thumbnailURL": "https://picsum.photos/400",
+    "categories_id": [
+      2,
+      3
+    ],
+    "imagesURL": [
+      "https://picsum.photos/300/500",
+      "https://picsum.photos/1000/400",
+      "https://picsum.photos/700/300"
+    ]
+  }, {
+    "_id": 7,
+    "artist_id": 1,
+    "name": "Florida Hedgenettle",
+    "shortDescription": "ut labore et dolore magna aliqua",
+    "price": 2239,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/500",
+    "categories_id": [
+      3
+    ],
+    "imagesURL": [
+      "https://picsum.photos/200/300",
+      "https://picsum.photos/300/500"
+    ]
+  }, {
+    "_id": 8,
+    "artist_id": 2,
+    "name": "Polianthes",
+    "shortDescription": "sed do eiusmod tempor incididunt",
+    "price": 243,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/300/500",
+    "categories_id": [
+      3
+    ],
+    "imagesURL": [
+      "https://picsum.photos/500/350",
+      "https://picsum.photos/500",
+      "https://picsum.photos/1000/1200"
+    ]
+  }, {
+    "_id": 9,
+    "artist_id": 3,
+    "name": "Darkbrown Sedge",
+    "shortDescription": "sed do eiusmod tempor incididunt",
+    "price": 1661,
+    "status": "sold",
+    "thumbnailURL": "https://picsum.photos/500/350",
+    "categories_id": [
+      1,
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/300/500",
+      "https://picsum.photos/500/350",
+      "https://picsum.photos/500"
+    ]
+  }, {
+    "_id": 10,
+    "artist_id": 1,
+    "name": "Mountain Bladderpod",
+    "shortDescription": "sed do eiusmod tempor incididunt",
+    "price": 1380,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/200/300",
+    "categories_id": [
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/500"
+    ]
+  }, {
+    "_id": 11,
+    "artist_id": 2,
+    "name": "Angled Pea",
+    "shortDescription": "consectetur adipiscing elit",
+    "price": 3508,
+    "status": "sold",
+    "thumbnailURL": "https://picsum.photos/1000/1200",
+    "categories_id": [
+      1
+    ],
+    "imagesURL": [
+      "https://picsum.photos/500",
+      "https://picsum.photos/700/300"
+    ]
+  }, {
+    "_id": 12,
+    "artist_id": 3,
+    "name": "Canyon Spiderwort",
+    "shortDescription": "sed do eiusmod tempor incididunt",
+    "price": 4893,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/700/300",
+    "categories_id": [
+      3,
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/700/300",
+      "https://picsum.photos/500",
+      "https://picsum.photos/400"
+    ]
+  }, {
+    "_id": 13,
+    "artist_id": 3,
+    "name": "Artocarpus",
+    "shortDescription": "consectetur adipiscing elit",
+    "price": 1710,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/500/350",
+    "categories_id": [
+      3
+    ],
+    "imagesURL": [
+      "https://picsum.photos/200/300"
+    ]
+  }, {
+    "_id": 14,
+    "artist_id": 2,
+    "name": "Mesa Tansyaster",
+    "shortDescription": "Lorem ipsum dolor sit amet",
+    "price": 1102,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/1000/1200",
+    "categories_id": [
+      3,
+      2
+    ],
+    "imagesURL": [
+      "https://picsum.photos/1000/400"
+    ]
+  }, {
+    "_id": 15,
+    "artist_id": 1,
+    "name": "Yellow Himalayan Raspberry",
+    "shortDescription": "Lorem ipsum dolor sit amet",
+    "price": 3998,
+    "status": "available",
+    "thumbnailURL": "https://picsum.photos/200/300",
+    "categories_id": [
+      1
+    ],
+    "imagesURL": [
+      "https://picsum.photos/200/300",
+      "https://picsum.photos/1000/400"
+    ]
+  }
+]
