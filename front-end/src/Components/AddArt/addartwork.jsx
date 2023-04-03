c6b283e86bdb251cf75cf75a0ad87531dac75647
StevenZhang0116 <zz2589@nyu.edu>
2023-04-02 22:00:15 -0400

    some changes

--- front-end/src/Components/AddArt/addartwork.jsx
+++ front-end/src/Components/AddArt/addartwork.jsx
@@ -1,12 +1,16 @@
-import React, {useEffect, useState, useMemo} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
+import { useParams } from 'react-router-dom'
 import './addartwork.css'
 import TextareaAutosize from 'react-textarea-autosize';
 import axios from 'axios'
+import AllCategories from '../../SchemaSamples/AllCategories'
+import Select from "react-select";
+import { useNavigate } from 'react-router-dom'
 
- 
 const AddArtWork = props => {
-    const [addrtype, setAddrtype] = useState([])
+    const navigate = useNavigate()
 
+    const [addrtype, setAddrtype] = useState([])
     const [file,setFile] = useState()
     const [file2,setFile2] = useState()
     const [file3,setFile3] = useState()
@@ -14,44 +18,23 @@ const AddArtWork = props => {
     const [thePrice,setPrice] = useState("")
     const [theNum,setNum] = useState("")
     const [theDescription,setTheDescription] = useState("")
-    
-    const testcase = ['comedy','laugh','smile'];
+    const [genres, setGenres] = useState([]);
 
-    const [selected, setSelected] = useState('')
+    const handleSkillChange = (genres) => {
+        setGenres(genres || []);
+    }
 
-    const apiUrl = "https://my.api.mockaroo.com/moviegenre.json?key=512c4f30"
-
-    const getGenre = async() => {
-        try{
-            const response = await axios.get(apiUrl)
-            console.log(`success: ${response.status}`)
-            const data = response.data
-            console.log(data)
-            data.forEach((obj,i,arr) => {
-                if (addrtype.length < 10){ 
-                    addrtype.push(obj.genre)
-                }
-            })
-            console.log(addrtype)
-        } catch(err){
-            console.log(`failure: ${err}`)
-        }
-    };
-
-    useEffect(() =>{
-        getGenre();
-    },[addrtype]);
+    var testcase = [];
+    for (var i = 0; i < AllCategories.length; i++){
+        testcase.push({'label': AllCategories[i].name, 'value': AllCategories[i]._id});
+    }
 
     /* check whether inputs are numeric */
     /* for PRICE input only */
     function checkNumber(val,indi){
         val = val.replace(/[^\d]/g, '');
-        if (indi === 1){
-            setPrice(val);
-        }
-        else if (indi === 2){
-            setNum(val);
-        }
+        if (indi === 1) setPrice(val); 
+        else if (indi === 2) setNum(val); 
     }
 
     /* check whether inputs are character/special character only */
@@ -63,34 +46,57 @@ const AddArtWork = props => {
     
     /* maximum 3 photos */ 
     function handleChange(event){
-        console.log(event.target.files)
         setFile(URL.createObjectURL(event.target.files[0]));
     }
 
     function handleChange2(event){
-        console.log(event.target.files)
         setFile2(URL.createObjectURL(event.target.files[0]));
     }
 
     function handleChange3(event){
-        console.log(event.target.files)
         setFile3(URL.createObjectURL(event.target.files[0]));
     }
 
-    function handleSubmit(event){
-        event.preventDefault()
-        /* test */ 
-        console.log('SUBMIT TEST')
-        console.log("Name:", theName)
-        console.log("Price:", thePrice)
-        console.log("Number:", theNum)
-        console.log("Description:", theDescription)
-        console.log("Genre:", selected)
-    }
-    
-    // if (addrtype.length === 0) return <div>loading...</div>
-    function refreshPage() {
-        window.location.reload(false);
+    /* submit function */
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+
+        var categoryresult = [];
+        for (var i = 0; i < genres.length; i++){
+            categoryresult.push(genres[i].value);
+        }
+
+        /* let the user to further edit */
+        if (theName.length === 0 || thePrice.length === 0 || theNum.length === 0 || theDescription.length === 0 /* check if the inputs are valid*/
+            || theNum === 0 || thePrice === 0 /* check if the numbers are positive */
+            || typeof(file) === 'undefined' /* check if at least one photo is uploaded */
+            || categoryresult.length === 0 /* no genre is select */
+            ){
+                alert('Incorrect Information Input. Please check the format and redo it. ')
+        }
+        /* successful adding of product */
+        else{
+            alert('You have successfully added an artwork! ')   
+            /* product profile */
+            const newProduct = {
+                _id: Math.random(),
+                artist_id: props.user._id,
+                name: theName,
+                shortDescription: theDescription,
+                price: thePrice,
+                status: "available",
+                thumbnailURL: "",
+                categories_id: categoryresult,
+                imagesURL: ["","",""]
+            }
+            /* check server here but for now just print user data to console
+            try{
+                await axios.post(...)
+            }
+            */
+            console.log(newProduct)  
+            navigate("/")       
+        }
     }
 
     return (
@@ -104,42 +110,45 @@ const AddArtWork = props => {
             <h4>
                 Maximum 3 Photos; Please Upload Sequentially. 
             </h4>
-            <div className='addworkset'>                
-                <input type="file" onChange={handleChange} />
-                <img className="photopic" src={file} />
-                <input type="file" onChange={handleChange2} />
-                <img className="photopic" src={file2} />
-                <input type="file" onChange={handleChange3} />
-                <img className="photopic" src={file3} />
-                <input type='text' placeholder='Name' value={theName} onChange={(e) => checkCharacter(e.target.value)} />
-                <input type='text' placeholder='Price' value={thePrice} onChange={(e) => checkNumber(e.target.value,1)} />
-                <input type='text' placeholder='Number' value={theNum} onChange={(e) => checkNumber(e.target.value,2)} />
-                <TextareaAutosize type='text' placeholder='Decription' value={theDescription} onChange={(e) => setTheDescription(e.target.value)} />
+            <div className='addworkset'>                 
+                <input type="file" onChange={handleChange}/>
+                <img className="photopic" src={file}/>
+                <input type="file" onChange={handleChange2}/>
+                <img className="photopic" src={file2}/>
+                <input type="file" onChange={handleChange3}/>
+                <img className="photopic" src={file3}/>
+
+                <input type='text' placeholder='Name: Characters Only' value={theName} onChange={(e) => checkCharacter(e.target.value)}/>
+                <input type='text' placeholder='Price: Digits Only' value={thePrice} onChange={(e) => checkNumber(e.target.value,1)}/>
+                <input type='text' placeholder='Number: Digits Only' value={theNum} onChange={(e) => checkNumber(e.target.value,2)}/>
+
+                <TextareaAutosize type='text' placeholder='Decription' value={theDescription} onChange={(e) => setTheDescription(e.target.value)} 
+                style={{ width: "25%", fontSize: 15}} minRows={2} maxRows={10}/>
                 <br/>
                 <br/>
                 <h3>
-                    Category Selection
+                    Category Selection(s)
                 </h3>
-
-                <form> 
-                    <select value={selected} onChange={e => setSelected(e.target.value)}>
-                        {testcase.map((value) => (
-                            <option value={value} key={value}>
-                                {value}
-                            </option>
-                        ))}
-                    </select>
-                </form>
+                <div style={{width:'300px', margin:'auto', fontSize:15}}>
+                    <Select 
+                        options={testcase} 
+                        onChange={handleSkillChange} 
+                        value={genres} 
+                        isMulti
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                        menuPlacement="auto"
+                        menuPosition="fixed"
+                    />
+                </div>
             
                 <form onSubmit={handleSubmit}>
-                    <input type='submit' value='save' />  
+                    <input type='submit' value='SUBMIT!' />  
                 </form>
             </div>
-        </div>
-        
+        </div>  
     );
 
 }
 
 export default AddArtWork
-
