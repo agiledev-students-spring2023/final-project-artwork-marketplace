0b3b8657b9b198254aba89316b9a8ec74ec05443
Anhtrr <at3947@nyu.edu>
2023-04-13 18:26:52 -0400

    finished all routes for users and tested

--- back-end/app.js
+++ back-end/app.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
-app.use("/static", express.static("public"))
+app.use("/static", express.static("Public"))
 
 mongoose
   .connect(`${process.env.DB_CONNECTION_STRING}`)
